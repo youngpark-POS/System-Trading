403804aa9706257debd77f462c90cd2230e062ef
youngpark <pnet575@gmail.com>
2020-08-01 13:44:34 +0900

    ver200801_1344

--- trading.py
+++ trading.py
@@ -31,6 +31,8 @@ class MyWindow(QMainWindow, ui_form):
         if errcode == 0:
             self.statusBar().showMessage("OK")
             self.textEdit.append("Login Success")
+            self.get_logininfo()
+            self.get_codelist(0)
         elif errcode == -100:
             self.statusBar().showMessage("User information exchange failed")
         elif errcode == -101:
@@ -46,13 +48,29 @@ class MyWindow(QMainWindow, ui_form):
                                 "coingo", "opt10001", 0, "0101")
 
     def receive_Trdata(self, *args):
-        if args[1] == "coingo": # args[1] is sRQName, args[2] is sTrcode
+        if args[1] == "coingo":  # args[1] is sRQName, args[2] is sTrcode
             name = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)",
                                            args[2], "", args[1], 0, "종목명")
             volume = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)",
                                              args[2], "", args[1], 0, "PER")
+
             self.textEdit.append("Item name : " + name.strip())
             self.textEdit.append("Volume : " + volume.strip())
+
+    def get_logininfo(self):
+        account_num = self.kiwoom.dynamicCall("GetLoginInfo(QString)", "ACCNO")
+        self.textEdit.append("Account number : " + ','.join(account_num.rstrip(';').split(';')))
+
+    def get_codelist(self, *args):
+        code_name_list = []
+        for arg in args:
+            code_list = self.kiwoom.dynamicCall("GetCodeListByMarket(QString)", str(arg)).split(';')
+            del code_list[-1]
+            for code in code_list:
+                name = self.kiwoom.dynamicCall("GetMasterCodeName(QString)", code)
+                code_name_list.append(code + " : " + name)
+        for code_name in code_name_list:
+            self.textEdit.append(code_name)
 
 
 if __name__ == "__main__":
@@ -60,3 +78,4 @@ if __name__ == "__main__":
     myWindow = MyWindow()
     myWindow.show()
     app.exec_()
+
