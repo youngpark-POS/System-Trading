1fc119326d49fcff967e7cdda66dcd4e6819018f
youngpark <pnet575@gmail.com>
2020-08-02 23:21:25 +0900

    ver.200802_2321

--- trading.py
+++ trading.py
@@ -5,77 +5,103 @@
 import sys
 
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5.QAxContainer import *
 from PyQt5 import uic
 import sqlite3
 
-ui_form = uic.loadUiType("new_main_window.ui")[0]
 
-
-class MyWindow(QMainWindow, ui_form):
+class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
-        self.setupUI()
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.kiwoom.dynamicCall("CommConnect()")
-        self.kiwoom.OnEventConnect.connect(self.event_connect)
-        self.textEdit.setEnabled(False)
-        self.searchButton.clicked.connect(self.search_item)
-        self.kiwoom.OnReceiveTrData.connect(self.receive_Trdata)
+        self._create_instance()
+        self._set_signals_slots()
 
-    def setupUI(self):
-        self.setupUi(self)
+    def _create_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def _set_signals_slots(self):
+        self.OnEventConnect.connect(self.event_connect)
+        self.OnReceiveData.connect(self.receive_Trdata)
+
+    def comm_connect(self):
+        self.dynamicCall("CommConnect()")
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
 
     def event_connect(self, errcode):
         if errcode == 0:
-            self.statusBar().showMessage("OK")
-            self.textEdit.append("Login Success")
-            self.get_logininfo()
-            self.get_codelist(0)
-        elif errcode == -100:
-            self.statusBar().showMessage("User information exchange failed")
-        elif errcode == -101:
-            self.statusBar().showMessage("Server access failed")
-        elif errcode == -102:
-            self.statusBar().showMessage("Version progress failed")
+            print("Connected")
+            # self.get_logininfo()
+        else:
+            print("Disconnected")
+        self.login_event_loop.exit()
 
     def search_item(self):
         code = self.lineEdit.text()
-        self.textEdit.append("Item Code : " + code)
-        self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
-        self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)",
+        self.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+        self.dynamicCall("CommRqData(QString, QString, int, QString)",
                                 "coingo", "opt10001", 0, "0101")
 
-    def receive_Trdata(self, *args):
-        if args[1] == "coingo":  # args[1] is sRQName, args[2] is sTrcode
-            name = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)",
-                                           args[2], "", args[1], 0, "종목명")
-            volume = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)",
-                                             args[2], "", args[1], 0, "PER")
+    def comm_rq_data(self, rqname, trcode, next, screen_no):
+        self.dynamicCall("CommRqData(QString, QString, int, QString", rqname, trcode, next, screen_no)
+        self.tr_event_loop = QEventLoop()
+        self.tr_event_loop.exec_()
 
-            self.textEdit.append("Item name : " + name.strip())
-            self.textEdit.append("Volume : " + volume.strip())
+    def receive_Trdata(self, *args):
+        if args[4] == '2':  # args[4] is next
+            self.remained_data = True
+        else:
+            self.remained_data = False
+
+        if args[1] == "coingo":  # args[1] is sRQName, args[2] is sTrcode
+            self._opt10081(args[1], args[2])
+
+        try:
+            self.tr_event_loop.exit()
+        except AttributeError:
+            pass
 
     def get_logininfo(self):
-        account_num = self.kiwoom.dynamicCall("GetLoginInfo(QString)", "ACCNO")
-        self.textEdit.append("Account number : " + ','.join(account_num.rstrip(';').split(';')))
+        account_num = self.dynamicCall("GetLoginInfo(QString)", "ACCNO")
 
     def get_codelist(self, *args):
         code_name_list = []
         for arg in args:
-            code_list = self.kiwoom.dynamicCall("GetCodeListByMarket(QString)", str(arg)).split(';')
+            code_list = self.dynamicCall("GetCodeListByMarket(QString)", str(arg)).split(';')
             del code_list[-1]
             for code in code_list:
-                name = self.kiwoom.dynamicCall("GetMasterCodeName(QString)", code)
+                name = self.dynamicCall("GetMasterCodeName(QString)", code)
                 code_name_list.append(code + " : " + name)
         for code_name in code_name_list:
-            self.textEdit.append(code_name)
+            print(code_name)
+
+    def set_input_value(self, id, value):
+        self.dynamicCall("SetInputValue(QString, QString)", id, value)
+
+    def _comm_get_data(self, code, realtype, fieldname, index, itemname):
+        ret = self.dynamicCall("CommGetData(QString, QString, QString, int, QString)",
+                               code, realtype, fieldname, index, itemname)
+        return ret.strip()
+
+    def _opt10081(self, rqname, trcode):
+        data_cnt = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
+
+        features = ["일자", "시가", "고가", "저가", "현재가", "거래량"]
+        for i in range(data_cnt):
+            data_list = []
+            for feature in features:
+                data_list.append(self._comm_get_data(trcode, "", rqname, i, feature))
+            print(*data_list)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = MyWindow()
-    myWindow.show()
-    app.exec_()
+    kiwoom = Kiwoom()
+    kiwoom.comm_connect()
+    kiwoom.get_codelist(0)
+    kiwoom.set_input_value("종목코드", "039490")
+    kiwoom.set_input_value("기준일자", "20200801")
+    kiwoom.set_input_value("수정주가구분", "1")
+    kiwoom.comm_rq_data("coingo", "opt10081", 2, "0101")
 
