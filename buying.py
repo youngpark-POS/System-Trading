d55c401757fd9b30bcd10f98ad958bd0cbdbf7c5
youngpark <pnet575@gmail.com>
2020-08-28 15:53:55 +0900

    Update buying.py

--- buying.py
+++ buying.py
@@ -13,9 +13,10 @@ from pandas import Series, DataFrame
 import pandas as pd
 import sqlite3
 import time
-import os,subprocess    # 프로세스 관련 모듈인데 혹시 몰라서 넣어놈
+import os, subprocess    # 프로세스 관련 모듈인데 혹시 몰라서 넣어놈
 
 TR_REQ_TIME_INTERVAL = 0.2
+
 
 class Kiwoom(QAxWidget):
     def __init__(self):
@@ -28,8 +29,7 @@ class Kiwoom(QAxWidget):
 
     def _set_signals_slots(self):
         self.OnEventConnect.connect(self.event_connect)
-        self.OnReceiveTrData.connect(self.receive_Trdata)
-
+        self.OnReceiveTrData.connect(self.receive_trdata)
 
     def comm_connect(self):
         self.dynamicCall("CommConnect()")
@@ -57,7 +57,7 @@ class Kiwoom(QAxWidget):
         self.tr_event_loop = QEventLoop()
         self.tr_event_loop.exec_()
 
-    def receive_Trdata(self, *args):
+    def receive_trdata(self, *args):
         if args[4] == '2':  # args[4] is next
             self.remained_data = True
         else:
@@ -92,7 +92,7 @@ class Kiwoom(QAxWidget):
                 self.ohlcv[feature_en].append(data_list[j])
 
 
-class Conditon(QAxWidget):
+class Condition(QAxWidget):
     def __init__(self):
         super().__init__()
         self._create_instance()
@@ -103,19 +103,52 @@ class Conditon(QAxWidget):
 
     def _set_signals_slots(self):
         self.OnReceiveConditionVer.connect(self.condition_search)
-        self.OnReceiveTrCondition.connect(self.condition_serach)
-        self.OnReceiveRealCondition.connect(self.result_condition)
+        self.OnReceiveTrCondition.connect(self.receive_condition)
+        self.OnReceiveRealCondition.connect(self.condition_search)
+
+    def get_condition(self):
+        condition_loaded = self.dynamicCall("GetConditionLoad()")
+        self.search_event_loop = QEventLoop()
+        self.search_event_loop.exec_()
+        if condition_loaded == 0:
+            print("Condition loading failed")
+        else:
+            self.search_event_loop.exit()
 
     def condition_search(self):
-        self.dynamicCall("GetConditionLoad()")
-        self.search_event_loop = QEventLoop()
-        self.search_event_loop.exec
-        self.dynamicCall("GetConditionNameList()")
+        self.cond_dict = {}
+        condition_list = self.dynamicCall("GetConditionNameList()").split(';')
+        del condition_list[-1]
+        for cond in condition_list:
+            cond_index, cond_name = cond.split('^')
+            self.cond_dict[cond_index] = cond_name
+
+        try:
+            self.search_event_loop.exit()
+        except AttributeError:
+            pass
 
     def result_condition(self, screen_no, condition_name, nIndex, nSearch):
-        self.dynamicCall("SendCondition(QString, QString, int, int)", screen_no, condition_name, nIndex, nSearch)
+        condition_sended = self.dynamicCall("SendCondition(QString, QString, int, int)", screen_no, condition_name, nIndex, nSearch)
+        if condition_sended == 0:
+            print("Condition sending failed")
 
         ## 조건검색 결과 가공
+
+    def send_condition(self, screen_no, cond_name, cond_index, search):
+        condition_sended = self.dynamicCall("SendCondition(QString, QString, int, int)", screen_no, cond_name, cond_index, search)
+        if condition_sended == 0:
+            print("Condition sending failed")
+
+    def receive_condition(self, screen_no, code_list, cond_name, cond_index, next):
+        if code_list == '':
+            return
+        sep_code_list = code_list.split(';')
+        self.item_list = []
+        for code in sep_code_list:
+            self.item_list.append(self.dynamicCall("GetMasterCodeName(QString)", code))
+
+
 
 
 if __name__ == "__main__":
@@ -143,20 +176,18 @@ if __name__ == "__main__":
     cursor = con_M.cursor()
     result1 = cursor.execute("SELECT * FROM table_001")    # 개별종목코드를 전체 시장코드로 변환
     row = result1.fetchmany(20)
-    bf = row[19][1]    # 20일 전 종가
-    nw = row[0][1]    # 현재 일봉 중 종가
-    bh = int(bf)  # 숫자열 변환
-    nl = int(nw)
+    bh = int(row[19][1])    # 20일 전 종가
+    nl = int(row[0][1])    # 현재 일봉 중 종가
     if nl > bh:
         print("1번 알고리즘을 실행합니다.")
 
         app = QApplication(sys.argv)   # 조건검색 알고리즘 1
-        condition = Conditon()
-        condition.condition_searh()
-        condition.result_condition("0150", "알고리즘 1", 0, 1)
+        condition = Condition()
+        condition.get_condition()
+        condition.result_condition("0150", condition.cond_dict['0'], 0, 1)
 
         con_C = sqlite3.connect("ConditionList.db")
-        df_C = DataFrame(condition.result_condition, columns=["code"], index=["1", "2", "3", "4", "5"])
+        df_C = DataFrame(condition.cond_dict, columns=["code"], index=["1", "2", "3", "4", "5"])
         df_C.to_sql("table_condition_result", con_C, if_exists="replace")
 
         cursors = con_C.cursor()
